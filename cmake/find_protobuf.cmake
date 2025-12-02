b634a3e33cbb0a4ea430887b7b6e088edb4506e6
drrtuy <drrtuy@gmail.com>
2025-12-02 22:13:43 +0200

    chore(cmake): properly set transitive lib dependencies for the target.

--- cmake/find_protobuf.cmake
+++ cmake/find_protobuf.cmake
@@ -1,3 +1,2 @@
-set(Protobuf_USE_STATIC_LIBS ON)
 find_package(Protobuf REQUIRED)
 message(STATUS "Using protobuf: ${Protobuf_VERSION} : ${Protobuf_INCLUDE_DIR}, ${Protobuf_LIBRARY}, ${Protobuf_PROTOC_EXECUTABLE}")
